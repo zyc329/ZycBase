823ede493458dd5bd06798b8176262dbdbb746c2
1165472536@qq.com <xtqsnss,1>
2020-09-27 22:43:56 +0800

    新增挂载在vue上方法的js和组件引入的config文件

--- ZycBaseCodeWeb/base-vue-web/src/config/index.js
+++ ZycBaseCodeWeb/base-vue-web/src/config/index.js
@@ -1,13 +1,2 @@
-// antd ui 组件样式引入
-import Vue from 'vue';
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
-import 'ant-design-vue/dist/antd.css';
-Vue.use(Antd);
-
-//lodash
-import _ from 'lodash'
-Vue.prototype._ = _
-
-
-
+import "./module"
+import "./functionUtil"
---
+++ ZycBaseCodeWeb/base-vue-web/src/config/module.js
@@ -0,0 +1,13 @@
+// antd ui 组件样式引入
+import Vue from 'vue';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import 'ant-design-vue/dist/antd.css';
+Vue.use(Antd);
+
+//lodash
+import _ from 'lodash'
+Vue.prototype._ = _
+
+
+
