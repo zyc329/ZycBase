ef346043620a97681e0f0638a96719f85ded33d2
1165472536@qq.com <xtqsnss,1>
2020-09-28 00:09:29 +0800

    axios引入

---
+++ ZycBaseCodeWeb/base-vue-web/src/utils/request.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import axios from 'axios'
+import store from '@/store'
+import nprogress from './nprogress'
+import notification from 'ant-design-vue/es/notification'
+
+notification.config({
+    placement: 'bottomRight'
+})
+
+// 创建 axios 实例
+const service = axios.create({
+    baseURL: '/api', // api base_url
+    timeout: 30000 // 请求超时时间
+})
+
+
