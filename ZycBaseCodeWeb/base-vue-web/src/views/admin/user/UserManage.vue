554cb9d0f9dc784c7e96f30d7cfe79e892fd619c
1165472536@qq.com <xtqsnss,1>
2020-09-28 09:34:24 +0800

    新建文件同步文件夹

---
+++ ZycBaseCodeWeb/base-vue-web/src/views/admin/user/UserManage.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "UserManage",
+        data() {
+            return {}
+        },
+        mounted() {
+
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ ZycBaseCodeWeb/base-vue-web/src/views/user/home/Index.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "Index",
+        data() {
+            return {}
+        },
+        mounted() {
+
+        },
+        methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
