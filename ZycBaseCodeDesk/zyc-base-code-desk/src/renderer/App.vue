389ba2fe7924dd7395a6feb46ef71d29cc7df897
1165472536@qq.com <xtqsnss,1>
2020-09-29 16:58:39 +0800

    no

---
+++ ZycBaseCodeDesk/zyc-base-code-desk/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'zyc-base-code-desk'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
