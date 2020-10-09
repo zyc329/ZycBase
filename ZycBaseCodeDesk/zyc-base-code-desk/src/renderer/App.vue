f951f10218f9e281be0d33a41c02bd71857a2ec4
zyc <1165472536@qq.com>
2020-10-09 09:00:28 +0800

    代码整理

---
+++ ZycBaseCodeDesk/zyc-base-code-desk/src/renderer/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'financial'
+  }
+</script>
+
+<style scoped>
+  /* CSS */
+  #app{
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+</style>
